96a2dd090dda89a3ba016a91606ed031525ddecd
Androm3d3 <44888228+Androm3d3@users.noreply.github.com>
2021-12-15 16:15:39 +0100

    Add files via upload

--- Project1/book.cpp
+++ Project1/book.cpp
@@ -1,29 +1,31 @@
 #include "book.h"
 
-Book::Book(std::string title, author::Author bookAuthor, std::string language, std::string category, date::Date pubDate, int isbn) : _title(title), _bookAuthor(bookAuthor), _language(language), _category(category), _pubDate(pubDate), _isbn(isbn) {
+namespace book {
+	Book::Book(std::string title, author::Author bookAuthor, std::string language, std::string category, date::Date pubDate, int isbn) : _title(title), _bookAuthor(bookAuthor), _language(language), _category(category), _pubDate(pubDate), _isbn(isbn) {
+		
+	}
 
-}
+	std::string Book::getTitle() const {
+		return _title;
+	}
 
-std::string Book::getTitle() const {
-	return _title;
-}
+	std::string Book::getAutor() {
+		return _bookAuthor.getFullname();
+	}
 
-std::string Book::getAutor() {
-	return _bookAuthor.getFullname();
-}
+	std::string Book::getLanguage() const {
+		return _language;
+	}
 
-std::string Book::getLanguage() const {
-	return _language;
-}
+	std::string Book::getCategory() const {
+		return _category;
+	}
 
-std::string Book::getCategory() const {
-	return _category;
-}
+	std::string Book::getPubDate() const {
+		return toString(_pubDate);
+	}
 
-std::string Book::getPubDate() const {
-	return toString(_pubDate);
-}
-
-int Book::getIsbn() const {
-	return _isbn;
+	int Book::getIsbn() const {
+		return _isbn;
+	}
 }

81aa1d711e985220f9f55cff30d9e10c81fb947b
Androm3d3 <44888228+Androm3d3@users.noreply.github.com>
2021-12-15 14:40:21 +0100

    Add files via upload
    question 2c

---
+++ Project1/book.h
@@ -0,0 +1,31 @@
+#include <string>
+#include "date.h"
+#include "author.h"
+
+#ifndef BOOK_H
+#define BOOk_H
+
+class Book
+{
+	public:
+		//constructeur "livre"
+		Book (std::string title, author::Author bookAuthor, std::string language, std::string category, date::Date pubDate, int isbn);
+		
+		//getters
+		std::string getTitle() const;
+		std::string getAutor();
+		std::string getLanguage() const;
+		std::string getCategory() const;
+		std::string getPubDate() const;
+		int getIsbn() const;
+
+	private:
+		std::string _title;
+		author::Author _bookAuthor;
+		std::string _language;
+		std::string _category;
+		date::Date _pubDate;
+		int _isbn;
+};
+
+#endif
